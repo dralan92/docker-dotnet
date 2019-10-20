fcfafc23b82da972035d3c70712083c9ce43aeeb
Alan Sebastian <alansebastian4@gmail.com>
2019-10-19 21:24:03 -0400

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcClient.Models;
+
+namespace MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult IndexAsync()
+        {
+            //var _client = new HttpClient();
+            //var requestMessage = new HttpRequestMessage(HttpMethod.Get, "http://localhost:5000/weatherforecast");
+            //var response = await _client.SendAsync(requestMessage);
+            //var messageString = await response.Content.ReadAsStringAsync();
+
+            ViewBag.Text = "Coming Soon";//messageString;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
